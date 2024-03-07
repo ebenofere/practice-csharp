465c4e1293eabc8028c573bae31858459b6dc0fc
ebenofere <eofere@gmail.com>
2024-03-06 22:25:50 -0500

    update

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -1,17 +1,13 @@
 ﻿// C# program to generate sequence of numbers within a range - get the even number and the squared numbers
 
-int[] numbers = { 1, 2, 3, 4, 5, 5, 6, 7, 8, 9 };
-
-// get the even number
-var evenNumber = numbers.Where(x => x % 2 == 0);
-var squaredNumber = numbers.Select(x => x * x);
-
-foreach (var item in evenNumber)
-{
-    Console.WriteLine(item);
-}
-
-foreach(var item in squaredNumber)
+int[] numberList = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+var evenArray = numberList.Where(x => x % 2 == 0);
+var squaredArray = numberList.Select(x => x * x);
+//foreach(int item in evenArray)
+//{
+//    Console.WriteLine(item);
+//}
+foreach (int item in squaredArray)
 {
     Console.WriteLine(item);
 }
