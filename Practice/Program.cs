6c2853953d87fb38c3c4a31914c356920fc0a06c
ebenofere <eofere@gmail.com>
2024-03-09 00:32:15 -0500

    update

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -10,3 +10,14 @@ else
 {
     Console.WriteLine("The number entered is not divisible by 2");
 }
+
+Console.WriteLine("Enter the number");
+n = Convert.ToInt32(Console.Read());
+if (n % 2 == 0)
+{
+    Console.WriteLine("The number entered is divisible by 2");
+}
+else
+{
+    Console.WriteLine("The number entered is not divisible by 2");
+}
