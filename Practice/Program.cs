97499c42a0f285c3321e437990bdb9b6eeec26c6
ebenofere <eofere@gmail.com>
2024-05-16 02:14:30 -0400

    update

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -1,7 +1,18 @@
-﻿// This is a C# program to generate sequence of numbers within a range
+﻿// this is a c# program to generate sequence of numbers within a range - get  the even number and then the squared numbers too
 
-IEnumerable<int> GenerateNumber = new List<int> { 1, 3, 4, 5, 6, 7, 8, 9 };
-foreach (int n in GenerateNumber)
+int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+// examples of using Enumerable extension methods
+
+var evenNumbers = numbers.Where(n => n % 2 == 0);
+var squaredNumbers = numbers.Select(x => x * x);
+
+foreach (var item in evenNumbers)
 {
-    Console.WriteLine(n);
+    Console.WriteLine(item);
+}
+
+foreach (var item in squaredNumbers)
+{
+    Console.WriteLine(item);
 }
