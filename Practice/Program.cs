b3847a5e3cef9f606a78c498904b266e05552105
ebenofere <eofere@gmail.com>
2024-05-16 02:00:34 -0400

    update

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -1,5 +1,6 @@
 ﻿// This is a C# program to generate sequence of numbers within a range
-IEnumerable<int> GenerateNumber = new List<int> { 1, 2, 3, 5, 3, 2, 7, 8 };
+
+IEnumerable<int> GenerateNumber = new List<int> { 1, 3, 4, 5, 6, 7, 8, 9 };
 foreach (int n in GenerateNumber)
 {
     Console.WriteLine(n);
