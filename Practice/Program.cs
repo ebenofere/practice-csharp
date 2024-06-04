133b35962510754dbd1de9ca0493a05fe44d0a8e
ebenofere <eofere@gmail.com>
2024-06-04 15:53:30 -0400

    update

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -1,17 +1,1 @@
-﻿// this is a c# program to generate sequence of numbers within a range - get the even number and then the squared numbers too
-
-int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-
-//get the even number
-var evenNumbers = numbers.Where(item => item % 2 == 0);
-var squaredNumbers = numbers.Select(item => item * item);
-
-foreach(int n in evenNumbers)
-{
-    Console.WriteLine(n);
-}
-
-foreach(int n in squaredNumbers)
-{
-    Console.WriteLine(n);
-}
+﻿// C# program to get a sum and display the sum of the digits
