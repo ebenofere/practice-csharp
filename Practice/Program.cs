433a3b6ee2529b94595b24a55f8906727e4a17f3
ebenofere <eofere@gmail.com>
2024-06-04 01:24:09 -0400

    update

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -1,5 +1,5 @@
 ﻿// this is a C# program numbers sequence of numbers within a range.
-IEnumerable<int> oddNums = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+IEnumerable<int> oddNums = new List<int> { 1, 2, 3, 4, 5 };
 foreach (int items in oddNums)
 {
     Console.WriteLine(items);
