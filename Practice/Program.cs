ff34c74d8761f18f5eacddda62935069a8e6b0b3
ebenofere <eofere@gmail.com>
2024-06-04 01:20:27 -0400

    update

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -1,5 +1,5 @@
-﻿// this is a c# program to generate odd numbers within a range
-IEnumerable<int> oddNums = Enumerable.Range(1, 100).Where(x => x % 2 != 0);
+﻿// this is a C# program numbers sequence of numbers within a range.
+IEnumerable<int> oddNums = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 foreach (int items in oddNums)
 {
     Console.WriteLine(items);
