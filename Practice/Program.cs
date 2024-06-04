d8571f93a72ecd04aae32e6c457ec73e8f400031
ebenofere <eofere@gmail.com>
2024-06-04 01:32:38 -0400

    update

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -1,7 +1,1 @@
-﻿// this is a C# program numbers sequence of numbers within a range.
-IEnumerable<int> oddNums = new List<int> { 1, 2, 3, 4, 5 };
-foreach (int items in oddNums)
-{
-    Console.WriteLine(items);
-}
-Console.ReadLine();
+﻿// this is a c# program to generate sequence of numbers within a range - get the even number and then the squared numbers too
