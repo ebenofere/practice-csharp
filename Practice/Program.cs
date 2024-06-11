c081d0c6091366d519c3d4971f921b9306f710a7
ebenofere <eofere@gmail.com>
2024-06-11 10:46:56 -0400

    check  if the number is palindrome or not

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -1,15 +1,26 @@
-﻿//C# Program to Get a Number and Display the Number with its Reverse
+﻿// C# program to get a number and tell if it is a palindrome or not
 
-int num, reverse = 0;
-Console.WriteLine("enter the number : ");
+int num, temp, reverse = 0, remainder;
+Console.WriteLine("Enter a number : ");
 num = int.Parse(Console.ReadLine());
+temp = num;
 
-while (num != 0)
+while (num > 0)
 {
-    reverse = reverse * 10;
-    reverse = reverse + (num % 10);
-    num /= 10;
+    remainder = num % 10;
+    reverse = (reverse * 10) + remainder;
+    num = num / 10;
 }
 
-Console.WriteLine($"the reverse of the entered number is {reverse}");
+Console.WriteLine($"the number you entered is {temp}");
+Console.WriteLine($"the reverse is {reverse}");
+
+if (temp == reverse)
+{
+    Console.WriteLine("the number you entered is a palindrome");
+}
+else
+{
+    Console.WriteLine("the number you entered is not a palindrome");
+}
 Console.ReadLine();
