1f450780b4b3e460c21cd4512a2cba135a2811de
ebenofere <eofere@gmail.com>
2024-06-10 23:55:02 -0400

    update

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -1,1 +1,10 @@
 ﻿// C# program to get a sum and display the sum of the digits
+
+int a, b, sum;
+Console.WriteLine("enter the first number");
+a = int.Parse(Console.ReadLine());
+Console.WriteLine("enter the second number");
+b = int.Parse(Console.ReadLine());
+sum = a + b;
+Console.WriteLine($"The total is {sum}");
+Console.Read();
