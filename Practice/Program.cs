6a887d41f3188f57398aede6797011d715a36648
ebenofere <eofere@gmail.com>
2024-07-29 23:58:02 -0400

    update

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿// C# program to check whether the entered number is even or odd
+
+int i;
+Console.Write("Enter a number : ");
+i = int.Parse(Console.ReadLine());
+if (i % 2 == 0)
+{
+    Console.WriteLine("the entered number is an even number");
+    Console.Read();
+}
+else
+{
+    Console.WriteLine("the entered number is an odd number");
+    Console.Read();
+}
